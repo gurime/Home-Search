223698fab0be00a1e88ee5796403e6827f04847a
Phillip Bailey <ratshady@comcast.net>
2020-08-13 12:28:22 -0400

    Add files via upload

---
+++ Gracious-apartments/main.js
@@ -0,0 +1,7 @@
+function openSidenav(){
+document.getElementById('sidenav').style.width = "320px";
+document.getElementById('sidenav').style.top = "0";
+}
+function closeSidenav(){
+document.getElementById('sidenav').style.width = "0px";
+}
